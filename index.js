cc40e5c9951b4a26be0658e39538d73cc7e8b88b
Tajbir islam <studenttajbirislam@gmail.com>
2024-04-27 19:15:52 +0600

    add vercel file

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const cors = require('cors')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173'
+}));
 
 app.use(express.json());
 
